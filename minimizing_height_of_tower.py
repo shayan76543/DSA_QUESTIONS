b9ddf07491f9fc58ae598f86caec3a453d8b7aee
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-02-19 05:26:40 +0500

    minimizing the height of tower is added succesfully

---
+++ minimizing_height_of_tower.py
@@ -0,0 +1,21 @@
+def minimizing_height():
+    arr=[1,2,3,4]
+    k=2
+    n=len(arr)
+    arr.sort()
+    ans=arr[-1]-arr[0]
+    new_min=arr[0]+k
+    new_max=arr[-1]-k
+    if new_max<new_min:
+        new_max,new_min=new_min,new_max
+    for i in range(1,n-1):
+        sub=arr[i]-k
+        add=arr[i]+k
+        if sub>=new_min or add<=new_max:
+            continue
+        if (new_max-sub)<=(add-new_min):
+            new_min=sub
+        else:
+            new_max=add
+    return min(ans,new_max-new_min),new_min,new_max
+print(minimizing_height())          
