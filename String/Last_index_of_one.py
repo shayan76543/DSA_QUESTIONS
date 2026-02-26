81ace4de915c08fad177681bfc75b365271e4e0c
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-02-26 14:46:55 +0500

    Correct the file Format

--- String/Last_index_of_one.py
+++ String/Last_index_of_one.py
@@ -1,10 +1,10 @@
 def Last_index_of_one():
-    string="00000001"
-    n=len(string)
-    Last_index=None
-    for i in range(0,n):
-        if string[i]=="1":
-            Last_index=i
+    string = "00000001"
+    n = len(string)
+    Last_index = None
+    for i in range(0, n):
+        if string[i] == "1":
+            Last_index = i
     if Last_index is None:
         return -1
     else:
