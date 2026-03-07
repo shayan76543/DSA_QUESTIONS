de2cfb4439579d00f8cd04cb48b58f34fb2827b3
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-03-07 11:35:57 +0500

    Add some functionality in parenthesis checker question

--- String/parenthesis_checker.py
+++ String/parenthesis_checker.py
@@ -1,23 +1,18 @@
-def parenthesis_checker():
-    brackets="{[(]})"
+def parenthesis_checker(bracket):
     stack=[]
-    for i in brackets:
-        if i in "([{":
+    for i in bracket:
+        if i in "({[":
             stack.append(i)
         else:
             if len(stack)==0:
                 return False
             p=stack.pop()
             if (
-                p=="(" and i==")"
-                or p=="{" and i=="}" 
-                or p== "[" and i=="]"
+                p=="(" and i==")" or 
+                p=="{" and i=="}" or 
+                p=="[" and i=="]"
             ):
                 continue
-            else:
-                return False
+            return False
     return len(stack)==0
-print(parenthesis_checker())    
-
-
-
+print(parenthesis_checker(bracket=("([]})")))
