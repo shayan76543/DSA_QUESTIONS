fff2f1ba28a150183394df86f58e005848b82c11
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-03-14 14:49:48 +0500

    change the name of longest common prfix  question to common string

--- String/longest_common_prefix.py
+++ String/longest_common_prefix.py
@@ -1,17 +1,14 @@
-def longest_common_prefix(arr):
+def longest_common_string(arr):
     if len(arr)==0:
         return ""
-    prefix=arr[0]
+    common_string=arr[0]
     for word in arr[1:]:
         i=0
-        while i<len(prefix) and i<len(word) and word[i]==prefix[i]:
+        while i<len(common_string) and i<len(word) and word[i]==common_string[i]:
             i+=1
-        prefix=word[:i]
-        if prefix == "":
+        common_string=word[:i]
+        if common_string == "":
             return ""
-    return prefix
-arr=["shayan","shaya","shan"]
-print(longest_common_prefix(arr))
-
-
-
+    return common_string
+arr=["h","he","hello"]
+print(longest_common_string(arr))
