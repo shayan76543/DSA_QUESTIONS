7c7c599eb9e925a5034456d2d8b423ec75bd2f34
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-03-18 19:43:03 +0500

    add longest sub string with no repatation

---
+++ String/longest_substring_without_repeating.py
@@ -0,0 +1,13 @@
+def non_repeating_longest_sub(string):
+    n=len(string)
+    left=0
+    max_len=0
+    box=set()
+    for right in range(0,n):
+        while string[right] in box:
+            box.remove(string[left])
+            left+=1
+        box.add(string[right])
+        max_len=max(max_len,right-left+1)
+    return max_len
+print(non_repeating_longest_sub("khawshd"))
