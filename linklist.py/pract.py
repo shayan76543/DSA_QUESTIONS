b841cae005ab25badd050c42602b8bcfd9e5b129
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-04-01 19:24:40 +0500

    practice question of linklist

---
+++ linklist.py/pract.py
@@ -0,0 +1,49 @@
+class node:
+    def __init__(self,info,address=None):
+        self.info=info
+        self.address=address
+class linklist:
+    def __init__(self,head=None):
+        self.head=head
+    def insert_beggining(self,value):
+        temp=node(value)
+        t1=self.head
+        temp.address=self.head
+        self.head=temp
+    def insert_mid(self,value,x):
+        temp=node(value)
+        t1=self.head
+        while (t1!=None):
+            if (t1.info==x):
+                temp.address=t1.address
+                t1.address=temp
+            t1=t1.address
+    def insert_end(self,value):
+        temp=node(value)
+        if self.head!=None:
+            t1=self.head
+            while (t1.address!=None):
+                t1=t1.address
+            t1.address=temp
+        else:
+            self.head=temp
+    def print(self):
+        t1=self.head
+        while t1!=None: # here we used this to check wether our node is exit or not 
+            print(t1.info)
+            t1=t1.address
+first_list=linklist()
+first_list.insert_end(1)
+first_list.insert_end(2)
+first_list.insert_end(3)
+first_list.insert_end(11)
+first_list.insert_end(5)
+first_list.insert_end(6)
+first_list.insert_end(7)
+first_list.insert_beggining(0)
+first_list.insert_mid(4,3)
+first_list.print()
+
+
+
+    
