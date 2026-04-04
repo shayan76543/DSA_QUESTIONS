6c148660d69d6b18049b6e7ff09f90cae63a6517
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-04-04 11:55:31 +0500

    add some function

--- linklist.py/linklist_crud_operations.py
+++ linklist.py/linklist_crud_operations.py
@@ -47,6 +47,14 @@ class linklist:
         while t1!=None: # here we used this to check wether our node is exit or not 
             print(t1.info)
             t1=t1.address
+    def lenght_counting(self): # for length counting of linklist
+        t1=self.head
+        length=0
+        while t1 is not None:
+            length+=1
+            t1=t1.address
+        return length
+
 first_list=linklist()
 first_list.insert_end(1)
 first_list.insert_end(2)
@@ -59,6 +67,7 @@ first_list.insert_beggining(0)
 first_list.insert_mid(4,3)
 first_list.delete_ll(7)
 first_list.print()
+print(first_list.lenght_counting())
 
 
 
