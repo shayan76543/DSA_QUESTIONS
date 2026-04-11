3db757311885a86feb9d566cba3f303a1ec74eb3
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-04-11 11:59:04 +0500

    i have done all crud operation of array as a practice

--- Dsa_Revisions_questions/array_crud.py
+++ Dsa_Revisions_questions/array_crud.py
@@ -5,4 +5,24 @@ def array_create():
         i=int(input("Entere Number:"))
         array.append(i)
     return array
-def array_read()
+def array_read():
+    arr=[4,6,7,1,2,4]
+    n=len(arr)
+    for i in range(0,n):
+        print(arr[i],end=" array Elements -->")
+    print("Stop")
+def update_array():
+    arr=[3,6,7,3,11,9]
+    arr[3]=23
+    return arr
+def delete_array():
+    arr=[4,6,7,8,9,23,56]
+    n=len(arr)
+    value=int(input("Entere your value:"))
+    if value in arr:
+        index=arr.index(value)
+        for i in range (index,n-1):
+            arr[i]=arr[i+1]
+        arr.pop()
+    return arr
+
