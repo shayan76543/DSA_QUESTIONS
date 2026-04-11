b03342f1ddad5a4982ce9033b95c05f083c32bff
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-04-11 16:42:01 +0500

    create class for crud operation of list

--- Dsa_Revisions_questions/array_crud.py
+++ Dsa_Revisions_questions/array_crud.py
@@ -25,4 +25,39 @@ def delete_array():
             arr[i]=arr[i+1]
         arr.pop()
     return arr
+class array_crud:
+    def __init__(self):
+        self.list=[]
+    def add_value_array(self):
+        n=int(input("Entere how many numbers you wanna insert in list:"))
+        for i in range(0,n):
+            value=int(input("Entere array Elements:"))
+            self.list.append(value)
+    def read_array(self):
+        for i in range(0,len(self.list)):
+            print(self.list[i],end="<-->")
+    def update_array(self):
+        i=int(input("Entere index of the value:"))
+        if 0<=i<len(self.list):
+            value=int(input("entere value which you want to insert:"))
+            self.list[i]=value
+        else:
+            print("index out of range:")
+    def delete_array(self):
+        value=int(input("Entere number which you want to delete:"))
+        if value in self.list:
+            index=self.list.index(value)
+            for i in range(index,(len(self.list))-1):
+                self.list[i]=self.list[i+1]
+            self.list.pop()
+        else:
+            print("value are not found in list")
+        # return self.list
+l1=array_crud()
+l1.add_value_array()
+l1.update_array()
+l1.delete_array()
+l1.read_array()
+
+
 
