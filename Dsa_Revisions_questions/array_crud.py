ab423848681742fbf67d4f8b48876bd7c45be918
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-04-11 11:21:24 +0500

    create changes in name of linklist file

---
+++ Dsa_Revisions_questions/array_crud.py
@@ -0,0 +1,8 @@
+def array_create():
+    array=[]
+    n=int(input("Entere number of array you want to entere:"))
+    for i in range(0,n):
+        i=int(input("Entere Number:"))
+        array.append(i)
+    return array
+def array_read()
