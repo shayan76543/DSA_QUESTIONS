983f987698b4ffb130a54d6046f044dc283892b5
shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-04-30 11:51:48 +0500

    binary search traversing or binery search tree insertion

--- tree/traversing_of_tree.py
+++ tree/traversing_of_tree.py
@@ -3,11 +3,29 @@ class node:
         self.data=data
         self.left=None
         self.right=None
+# preorder Traversing
+# Rule: Root → Left → Right
 def preorder(root):
     if root!=None:
         print(root.data,end=" ")
         preorder(root.left)
         preorder(root.right)
+# inorder Traversing 
+# Rule:
+# Left → Root → Right
+def inorder(root):
+    if root!=None:
+        inorder(root.left)
+        print(root.data,end=" ")
+        inorder(root.right)
+# postorder traversing
+# Rule:
+# Left → Right → Root
+def postorder(root):
+    if root!=None:
+        postorder(root.left)
+        postorder(root.right)
+        print(root.data,end=" ")
 root=node(22)
 root.left=(node(33))
 root.right=(node(44))
@@ -16,3 +34,7 @@ root.left.right=(node(35))
 root.right.left=(node(45))
 root.right.right=(node(46))
 preorder(root)
+print()
+inorder(root)
+print()
+postorder(root)
