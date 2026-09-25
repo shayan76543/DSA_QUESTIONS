1177f669cbe0ee7351bc1e907af95cd47f5cde61
Shayan Ahmad <dermatologistimranahmad@gmail.com>
2026-09-25 17:56:37 +0500

    construct target array with multiple sum  question of heap

---
+++ heap/Construct_target_array.py
@@ -0,0 +1,25 @@
+import heapq
+class Solution:
+    def isPossible(self, target: list[int]) -> bool:
+        result=0
+        total=sum(target)
+        heap=[-x for x in target]
+        heapq.heapify(heap)
+        while True:
+            largest=-heapq.heappop(heap)
+            if largest==1:
+                return True
+            result=total-largest
+            if result==1:
+                return True
+            if result==0 or largest<=result:
+                return False
+            previous=largest%result
+            if previous==0:
+                return False
+            heapq.heappush(heap,-previous)
+            total=result+previous
+target=[9,3,5]
+try1=Solution()
+
+print(try1.isPossible(target))
